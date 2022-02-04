92c10d89288ad2ae3e2c90b43c1fa3a4b5151fd5
wg <1134614268@qq.com>
2022-02-04 21:02:44 +0800

    u

--- centos7/nodejs-install.sh
+++ centos7/nodejs-install.sh
@@ -4,6 +4,6 @@ wget https://npm.taobao.org/mirrors/node/v12.13.0/node-v12.13.0-linux-x64.tar.xz
 tar -xvf node-v12.13.0-linux-x64.tar.xz
 mv node-v12.13.0-linux-x64 /usr/local
 
-ln -s /usr/local/node-v12.13.0-linux-x64/bin/npm /usr/local/bin/
-ln -s /usr/local/node-v12.13.0-linux-x64/bin/node /usr/local/bin/
+ln -s /usr/local/node-v12.13.0-linux-x64/bin/npm /usr/bin/
+ln -s /usr/local/node-v12.13.0-linux-x64/bin/node /usr/bin/
 
