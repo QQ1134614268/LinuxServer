333e7fc0cfa919371f32214b8a1afc8c43e9f67f
黄冉 <huangran@szcentral.com>
2022-12-09 09:23:23 +0800

    u

--- world/world.sh
+++ world/world.sh
@@ -1,6 +1,6 @@
 # docker环境
 # git
-# mysql
+# mysql 优化内存
 # nginx代理转发
 # redis
 # nodejs编译
@@ -24,4 +24,15 @@ docker run -it --name world \
   -v /opt/world/data/log /home/world/data/log \
   -d world:1.0.1
 
+# 一键打包部署 https://blog.csdn.net/qq_29170455/article/details/125558065
+
 #每次更新--重新build,install依赖 -- 使用挂载模式 ?? 细小的优化,定制化?
+# 官方软件-- 稳定, 安装迅速, 意外情况少,可脱离docker
+# sdk工具包,稳定,可脱离docker
+# 代码不稳定,反复升级,bug修复, 依赖环境, 适合docker容器化
+
+# docker 以镜像为主, 非临时build镜像
+
+# 要不要nginx; nginx提供转发,代理文件服务器, 可删除
+# 挂载资源目录 data
+# 配置文件,环境变量读取功能
