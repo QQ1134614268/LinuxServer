2c8e4f3972bfefbbed3cb5060434dfdc910e4036
黄冉 <huangran@szcentral.com>
2022-12-19 18:40:16 +0800

    u

--- world/4.world-web.sh
+++ world/4.world-web.sh
@@ -4,18 +4,21 @@
 # eg: java源码,打包部署, 所以使用镜像??
 
 # node 环境编译, nginx代理 | 本地打包??( no )
+docker pull nginx
+
 npm run xxx
 
 echo "# dockerfile
-from  nginx:xxx
-cp dist xxx
+FROM nginx
+COPY dist/  /usr/share/nginx/html/
 " > xx.dockerfile
 
+docker build -t world-web:1.0 .
+docker run --name world-web -d -p 9020:80 world-web:1.0
+
+#
 echo "# nginx
 listen 21001
-
 " > xx.nginx.conf
 
-docker build
-
-docker run
+docker run --name nginx -d -p 9020:80 nginx
