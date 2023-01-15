d264f9971c33b1e0afb1deadd676547684e96fd5
wg <1134614268@qq.com>
2023-01-16 00:57:44 +0800

    u

--- world/4.world-web.sh
+++ world/4.world-web.sh
@@ -3,19 +3,16 @@ FROM node:14.15.0-slim AS myBuildImg
 # 添加源码
 # RUN echo '开始添加源码'
 COPY . /root/web
-# RUN cd /root/web
 WORKDIR /root/web
-# RUN npm -g install npm@7.19.1
-RUN npm install --force
-RUN npm run build
+RUN npm install
+RUN npm run build-ggok
 
 # 部署
 FROM nginx:1.23.3
-#copy jar from the first stage
 COPY --from=myBuildImg /root/web/dist /usr/share/nginx/html/
 CMD nginx -g 'daemon off;'" > web.dockerfile
 
-docker build -t web:1.0 -f web.dockerfile world/web/dist
+docker build -t web:1.0 -f web.dockerfile .
 
 # 配置文件
-docker run -itd --name web-world -p 9020:80 web:1.0
+docker run -itd --name web:1.0  -p 8010:80 --privileged=true --restart=always web:1.0
