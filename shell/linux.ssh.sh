b1cc2a8e52a331e80d6d1862d066cdae79fa2fdd
黄冉 <huangran@szcentral.com>
2023-07-04 10:16:59 +0800

    u

--- shell/linux.ssh.sh
+++ shell/linux.ssh.sh
@@ -1,17 +1,12 @@
-# 免密登录
-# 生成ssh密钥
-ssh-keygen -t rsa
-#三次enter 生成密钥 id_rsa id_rsa.pub
-
-# 追加 A机器公钥(id_rsa.pub) 到 B机器上的authorized_keys文件( ~/.ssh/authorized_keys)
-追加 A机器 ~/.ssh/id_rsa.pub 到 B机器 .ssh/authorized_keys
-
-# 追加 B机器public key 到 A机器器的known_hosts
-追加 44.39.17.51 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKNNGfPFAdkrxxeF0UGonELVUYWULrDrfm2qg0FiuqXj 到 ~/.ssh/known_hosts
+# 免密登录(从A -> 到) scp A:/file B:/file
+# 1. A机器 生成ssh密钥: ssh-keygen -t rsa ; 三次enter 生成 id_rsa id_rsa.pub
+# 2. 追加 A机器 ~/.ssh/id_rsa.pub 内容 到 B机器 ~/.ssh/authorized_keys
+# 3. 首次连接, 需要 识别 host
+# 方案1: A机器手动执行: scp A:/file B:/file , 会提示 unknown host, 确认后悔 向 ~/.ssh/known_hosts 输入数据
+# 方案2: 追加 B机器 id_rsa.pub 到 A机器的 known_hosts
 
 # 验证1: ssh A机器 传输到 B机器
 scp scp -r /root/test.txt root@192.168.251.10:/root/test.txt
-
 # 验证2: 登陆B机器, 无需密码 直接登录成功
 ssh 192.168.251.10
 
