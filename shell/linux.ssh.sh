95023e6631c671f31f7b89d51763313a6103ee76
黄冉 <huangran@szcentral.com>
2023-09-11 09:19:10 +0800

    u

--- shell/linux.ssh.sh
+++ shell/linux.ssh.sh
@@ -2,11 +2,11 @@
 # 1. A机器 生成ssh密钥: ssh-keygen -t rsa ; 三次enter 生成 id_rsa id_rsa.pub
 # 2. 追加 A机器 ~/.ssh/id_rsa.pub 内容 到 B机器 ~/.ssh/authorized_keys
 # 3. 首次连接, 需要 识别 host
-# 方案1: A机器手动执行: scp A:/file B:/file , 会提示 unknown host, 确认后悔 向 ~/.ssh/known_hosts 输入数据
+# 方案1: A机器手动执行: scp A:/file B:/file , 会提示 unknown host, 确认后 向 ~/.ssh/known_hosts 输入数据
 # 方案2: 追加 B机器 id_rsa.pub 到 A机器的 known_hosts
 
 # 验证1: ssh A机器 传输到 B机器
-scp scp -r /root/test.txt root@192.168.251.10:/root/test.txt
+scp scp -p 22 -r /root/test.txt root@192.168.251.10:/root/test.txt
 # 验证2: 登陆B机器, 无需密码 直接登录成功
 ssh 192.168.251.10
 
