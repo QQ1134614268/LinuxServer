c2658d147a01a08d04d3f53f34ccf5f7f7b2f4b3
wg <1134614268@qq.com>
2024-02-02 15:02:44 +0800

    u

--- docker/k8s/ReadMe.sh
+++ docker/k8s/ReadMe.sh
@@ -1,20 +1,28 @@
 容器化技术:
-  docker:
+  Docker:
   LXC是一种轻量级虚拟化技术，可以提供隔离和安全性
+  Containerd
+容器化平台:
+  k8s: 容器编排系统, 非docker运行
+  docker-compose: 容器编排; 场景: 少量容器; 无UI, 无监控
+  rancher:
+
+CI/CD:
+  工具:
+    Jenkins
+    gitlab/CICD
+  作用:
+    自动化 测试
+    自动化 部署
+    可扩展 持续改进
+
 docker:
   docker客户端->docker服务端(dockerd)->containerd
   Docker利用containerd作为其容器运行时的组件，来提供对容器生命周期的管理和操作功能
   containerd从Docker项目中分离出来
   containerd实现了CRI规范,可以被k8s集成
 
-容器化平台:
-  k8s
-  docker-compose
-  rancher
-
-CI/CD:
-  Jenkins
-  gitlab/CICD
+调研/产品/需求/代码/测试/CICD
 
 需求指标:
   一键部署
--- docker/k8s/docker-compose.sh
+++ docker/k8s/docker-compose.sh
@@ -1,1 +1,7 @@
+### docker-compose
 yum -y install docker-compose
+
+docker-compose -f k8s.demo.redis_cluster.yml up
+docker-compose -d -f k8s.demo.redis_cluster.yml up
+
+### k8s
