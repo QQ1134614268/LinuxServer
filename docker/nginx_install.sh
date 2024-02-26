65b934220deda986df4bb9f0add561302ffcee78
wg <1134614268@qq.com>
2024-02-26 09:11:05 +0800

    u

--- docker/nginx_install.sh
+++ docker/nginx_install.sh
@@ -3,8 +3,8 @@
 # https证书自动续期, certbot需要本地安装nginx
 docker run -d --name nginx \
   -p 80:80 \
-  -p 443:443    \
-  -v /etc/nginx/nginx.conf:/etc/nginx/nginx.conf    \
-  -v /var/log/nginx:/var/log/nginx   \
-  --privileged   \
+  -p 443:443 \
+  -v /etc/nginx/nginx.conf:/etc/nginx/nginx.conf \
+  -v /var/log/nginx:/var/log/nginx \
+  --privileged \
   --restart=always nginx
