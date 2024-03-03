cff1f48193d1eaa722c74cf3fb4da029c5fb2d39
wg <1134614268@qq.com>
2024-03-03 23:53:17 +0800

    u

--- docker/k8s/ReadMe.sh
+++ docker/k8s/ReadMe.sh
@@ -1,6 +1,6 @@
 容器化技术:
   Docker:
-  LXC是一种轻量级虚拟化技术，可以提供隔离和安全性
+  LXC是一种轻量级虚拟化技术,可以提供隔离和安全性
   Containerd
 容器化平台:
   k8s: 容器编排系统, 非docker运行
@@ -18,7 +18,7 @@ CI/CD:
 
 docker:
   docker客户端->docker服务端(dockerd)->containerd
-  Docker利用containerd作为其容器运行时的组件，来提供对容器生命周期的管理和操作功能
+  Docker利用containerd作为其容器运行时的组件,来提供对容器生命周期的管理和操作功能
   containerd从Docker项目中分离出来
   containerd实现了CRI规范,可以被k8s集成
 
