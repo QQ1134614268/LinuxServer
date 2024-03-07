7410d3227771b7aeac914820c52a8b9e6c61f7fd
wg <1134614268@qq.com>
2024-03-07 18:44:23 +0800

    u

--- docker/mqtt_install.sh
+++ docker/mqtt_install.sh
@@ -4,6 +4,8 @@ docker pull emqx
 docker run -d --name mqtt \
   -p 1883:1883 \
   -p 8883:8883 \
+  -p 8083:8083 \
+  -p 8084:8084 \
   -p 18083:18083 \
   --privileged --restart=always emqx
 
@@ -12,6 +14,6 @@ docker run -d --name mqtt \
 
 # 1883：MQTT 协议端口
 # 8883：MQTT/SSL 端口
-# 8083：MQTT/WebSocket 端口
+# 8083：MQTT/Ws 端口
 # 8084：MQTT/Wss l端口
 # 18083：Dashboard 管理控制台端口
