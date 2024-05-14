f195d33143dea32e2adbd3d704bc31bbf98c76f3
wg <1134614268@qq.com>
2024-05-14 23:42:29 +0800

    u

--- jenkins/04.run.sh
+++ jenkins/04.run.sh
@@ -17,12 +17,10 @@ times=(1 1 1 1 1 1 1 1 1 1 1 1 8 1 1 1 1 1 1 1)
 #项目数量
 num=20
 
-function menu()
-{
+function menu() {
 	clear
 	echo "=============================="
-	if [ $ISDEBUG == 1 ]
-	then
+	if [ $ISDEBUG == 1 ]; then
 		echo "======调试模式已经打开========"
 	fi
 	echo -ne "1:\t全部重启$date\n"
@@ -36,13 +34,11 @@ function menu()
 	read option
 }
 
-function menu3()
-{
+function menu3() {
 	clear
 	echo "=============================="
-	for i in $(seq 1 $num)
-	do
-		echo -ne "$i:\t${projects[$i-1]}\n"
+	for i in $(seq 1 $num); do
+		echo -ne "$i:\t${projects[$i - 1]}\n"
 	done
 	echo -ne "q:\texit\n"
 	echo "=============================="
@@ -50,10 +46,8 @@ function menu3()
 	read option
 }
 
-function main()
-{
-	while [ 1 ]
-	do
+function main() {
+	while [ 1 ]; do
 
 		case $option in
 		1)
@@ -98,7 +92,7 @@ function main()
 	done
 }
 
-function returnToMenu(){
+function returnToMenu() {
 	echo '按任意键返回主菜单'
 	read 1 fff
 	menu
@@ -106,20 +100,17 @@ function returnToMenu(){
 }
 
 #启动或重新
-function main2()
-{
-	if [[ $option -ge 1 && $option -le $num ]] #数字
-	then
-        #停止
-		stopOne ${projects[$option-1]}
-		
-        #启动
-        startOne ${projects[$option-1]} ${projectsIp[$option-1]} ${times[$i-1]}
-		
-        #返回菜单
+function main2() {
+	if [[ $option -ge 1 && $option -le $num ]]; then #数字
+		#停止
+		stopOne ${projects[$option - 1]}
+
+		#启动
+		startOne ${projects[$option - 1]} ${projectsIp[$option - 1]} ${times[$i - 1]}
+
+		#返回菜单
 		returnToMenu
-	elif [ $option == 'q' ] #退出
-	then
+	elif [ $option == 'q' ]; then #退出
 		exit 0
 	else #其他重新输入
 		echo "输入错误，重新输入:"
@@ -129,19 +120,16 @@ function main2()
 }
 
 #停止
-function main3()
-{
-	if [[ $option -ge 1 && $option -le $num ]] #停止指定服务
-	then
-        #停止
-		stopOne ${projects[$option-1]}
-                
-        #返回
+function main3() {
+	if [[ $option -ge 1 && $option -le $num ]]; then #停止指定服务
+		#停止
+		stopOne ${projects[$option - 1]}
+
+		#返回
 		returnToMenu
-	elif [ $option == 'q' ]  #退出
-	then
+	elif [ $option == 'q' ]; then #退出
 		exit 0
-	else  #其他重新输入
+	else #其他重新输入
 		echo "输入错误，重新输入:"
 		read option
 		main2
@@ -149,11 +137,9 @@ function main3()
 }
 
 #列出服务
-function main4()
-{
-	for i in $(seq 1 $num)
-	do
-		echo -ne ${projects[$i-1]} "\t" ${projectsIp[$i-1]} "\n"
+function main4() {
+	for i in $(seq 1 $num); do
+		echo -ne ${projects[$i - 1]} "\t" ${projectsIp[$i - 1]} "\n"
 	done
 	echo '按任意键返回主菜单'
 	read 1 fff
@@ -161,78 +147,69 @@ function main4()
 }
 
 #停止所有服务
-function stopAll()
-{
-	for i in $(seq 1 $num)
-	do
-        #停止
-		stopOne ${projects[$i-1]}
+function stopAll() {
+	for i in $(seq 1 $num); do
+		#停止
+		stopOne ${projects[$i - 1]}
 	done
 	returnToMenu
 }
 
 #停止单个服务操作
-function stopOne()
-{
-    #检查是否有进程存在
-    #count=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|wc -l`
-    count=`ps -ef |grep java|grep $1|grep -v grep|wc -l`
+function stopOne() {
+	#检查是否有进程存在
+	#count=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|wc -l`
+	count=$(ps -ef | grep java | grep $1 | grep -v grep | wc -l)
 
-    #如果有进程，则删掉
-    while [ $count != 0 ]
-    do
-        #获取该程序的进程id
-        #boot_id=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|awk '{print $2}'`
-        boot_id=`ps -ef |grep java|grep $1|grep -v grep|awk '{print $2}'`
+	#如果有进程，则删掉
+	while [ $count != 0 ]; do
+		#获取该程序的进程id
+		#boot_id=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|awk '{print $2}'`
+		boot_id=$(ps -ef | grep java | grep $1 | grep -v grep | awk '{print $2}')
 
-        #杀掉该程序的进程
-        kill -9 $boot_id
+		#杀掉该程序的进程
+		kill -9 $boot_id
 
-        #再次检查是否还有进程存在
-        #count=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|wc -l`
-        count=`ps -ef |grep java|grep $1|grep -v grep|wc -l`
+		#再次检查是否还有进程存在
+		#count=`ps -ef |grep java|grep $projects_home/$1|grep -v grep|wc -l`
+		count=$(ps -ef | grep java | grep $1 | grep -v grep | wc -l)
 
-        echo "关闭$1成功"
-        sleep 3
-    done
+		echo "关闭$1成功"
+		sleep 3
+	done
 }
 
 #启动或重启所有服务
-function startAll()
-{
-	for i in $(seq 1 $num)
-	do
+function startAll() {
+	for i in $(seq 1 $num); do
 		#停止
-        stopOne ${projects[$i-1]}
-		
-        #启动
-		startOne ${projects[$i-1]} ${projectsIp[$i-1]} ${times[$i-1]}
+		stopOne ${projects[$i - 1]}
+
+		#启动
+		startOne ${projects[$i - 1]} ${projectsIp[$i - 1]} ${times[$i - 1]}
 	done
 	returnToMenu
 }
 
 #启动单个服务操作
-function startOne()
-{
- 	echo "正在启动 $1 ......"
+function startOne() {
+	echo "正在启动 $1 ......"
 	#调试
 	DEBUG_OPTS=""
-	if [ $ISDEBUG == 1 ];
-	then
-	echo "启动$1,远程调试端口号:$2"
+	if [ $ISDEBUG == 1 ]; then
+		echo "启动$1,远程调试端口号:$2"
 		DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$2"
 	fi
-		
+
 	#判断日志目录是否存在，不存在则创建
 	logdir=$projects_home/logs/$1
-	if [ ! -d $logdir ];
-	then
+	if [ ! -d $logdir ]; then
 		mkdir -p $logdir
-    fi
-		
+	fi
+
 	#启动
-    java -jar -server  -Dname=$1 -Duser.timezone=Asia/Shanghai -Xms$[$3*1024]m -Xmx$[$3*1024]m -Djava.security.egd=file:/dev/./urandom $DEBUG_OPTS -DprojectName=$1 $projects_home/$1/$1.jar  > $projects_home/logs/$1/$1.log 2>&1 &
-    echo "启动$1成功1---$(date +%F%n%T)"
+	java -jar -server -Dname=$1 -Duser.timezone=Asia/Shanghai -Xms$(($3 * 1024))m -Xmx$(($3 * 1024))m -Djava.security.egd=file:/dev/./urandom $DEBUG_OPTS -DprojectName=$1 $projects_home/$1/$1.jar >$projects_home/logs/$1/$1.log 2>&1 &
+	echo "启动$1成功1---$(date +%F%n%T)"
 }
 
 #显示菜单
