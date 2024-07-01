7193bbebf356124265a757caa9d5b4e172929b23
wg <1134614268@qq.com>
2024-07-01 13:31:05 +0800

    u

--- shell/linux-firewall.sh
+++ shell/linux-firewall.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+systemctl start firewalld
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl enable firewalld
+systemctl state firewalld
+
 firewall-cmd --list-all # 查看全部
 firewall-cmd --list-ports  # 查看开放的端口
-firewall-cmd --reload # 重启生效
-# systemctl start|stop|disable|enable|state firewalld
-#开放 3306, 8000-10000端口
+ # 重启生效
+firewall-cmd --reload
+#开放端口
 firewall-cmd --permanent --zone=public --add-port=3306/tcp
-firewall-cmd --permanent --zone=public --add-port=8000-10000/tcp
-#移除
+firewall-cmd --permanent --zone=public --add-port=20-20000/tcp
+#移除端口
 firewall-cmd --permanent --zone=public --remove-port=80/tcp
 # 	–zone #作用域
 # 	–add-port=80/tcp #添加端口，格式为：端口/通讯协议
