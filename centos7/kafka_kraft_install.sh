15440e71fc5dfd7a4f2b5138a0b234cde731c157
wg <1134614268@qq.com>
2024-07-14 20:26:08 +0800

    u

--- centos7/kafka_kraft_install.sh
+++ centos7/kafka_kraft_install.sh
@@ -1,36 +1,20 @@
-# 最新版本: 学习资料,bug资料,第三方库
-
-# 1. windows
-cd D:\dev\kafka_2.13-3.3.1
-# 修改 .\config\kraft\server.properties
-log.dirs=D:/dev/kafka_2.13-3.3.1/logs/kraft-combined-logs
-
-# 生成存储目录唯一id 3dTO5CPBTbO2EPqSqQR02g
-.\bin\windows\kafka-storage.bat random-uuid
-
+#  --------------------------windows kraft模式--------------------------------
+cd D:\dev\kafka_2.13-3.6.2
+# 1.1 修改 .\config\kraft\server.properties
+log.dirs=D:/dev/kafka_2.13-3.6.2/logs/kraft-combined-logs
+# 1.2 生成存储目录
+#.\bin\windows\kafka-storage.bat random-uuid
 # .\bin\windows\kafka-storage.bat format -t <uuid> -c .\config\kraft\server.properties
 .\bin\windows\kafka-storage.bat format -t 3dTO5CPBTbO2EPqSqQR02g -c .\config\kraft\server.properties
-
-# 启动
+# 1.3 启动
 .\bin\windows\kafka-server-start.bat .\config\kraft\server.properties
 
-# 1.2 windows
-cd D:\dev\kafka_2.13-3.3.1
 
-# 修改 .\config\server.properties
-log.dirs=D:/dev/kafka_2.13-3.3.2/logs/kafka-logs
-# .\config\zookeeper.properties
-log.dirs=D:/dev/kafka_2.13-3.3.2/logs/zookeeper-logs
+#  --------------------------Linux kraft模式--------------------------------
+wget https://downloads.apache.org/kafka/3.6.2/kafka_2.13-3.6.2.tgz
 
-.\bin\windows\zookeeper-server-start.bat config\zookeeper.properties
-.\bin\windows\kafka-server-start.bat .\config\server.properties
-
-# 2. Linux
-wget https://downloads.apache.org/kafka/3.3.1/kafka_2.13-3.3.1.tgz
-
-./bin/kafka-storage.sh random-uuid
-
+# ./bin/kafka-storage.sh random-uuid
 # ./bin/kafka-storage.sh format -t <uuid> -c ./config/kraft/server.properties
 ./bin/kafka-storage.sh format -t 3dTO5CPBTbO2EPqSqQR02g -c ./config/kraft/server.properties
-
+# 启动
 ./bin/kafka-server-start.sh ./config/kraft/server.properties
