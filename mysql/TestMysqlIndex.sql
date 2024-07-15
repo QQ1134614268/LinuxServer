307432b6356ccf52b91c73560cce986c85578a78
wg <1134614268@qq.com>
2024-07-15 11:51:07 +0800

    u

--- mysql/TestMysqlIndex.sql
+++ mysql/TestMysqlIndex.sql
@@ -1,31 +1,41 @@
 -- mysql index
-
-# 索引 过滤后数据量大, 且查询字段不在索引上, 全表扫描;
-# 即: 查询字段命中索引,走索引; 索引过滤后小数据量, 走索引;
-DROP TABLE if EXISTS test.`user`;
-CREATE TABLE test.`test_index`
+DROP TABLE if EXISTS user_t;
+CREATE TABLE user_t
 (
-    id         INT PRIMARY KEY,
-    name1      VARCHAR(60),
-    name2      VARCHAR(60),
-    createTime VARCHAR(60)
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(60),
+    nickName VARCHAR(60),
+    age      int,
+    KEY idx_name (name) USING BTREE,
+    KEY idx_birthday (age) USING BTREE
 );
--- range索引; 查询字段命中索引;
-EXPLAIN SELECT id  FROM test.`test_index` WHERE createTime >='2020-10-10 10:00:00' AND createTime<='2020-10-10 11:00:00' ;
-EXPLAIN SELECT name2  FROM test.`test_index` WHERE createTime>='2020-10-10 10:00:00' AND createTime<='2020-10-10 11:00:00' ;
--- range索引; 小数据量;
-EXPLAIN SELECT *  FROM test.`test_index` WHERE createTime>='2023-08-23 15:58:04.0' AND createTime<='2020-10-10 11:00:00' ;
+INSERT INTO user_t(name,nickName,age)VALUES('1','1',10),('2','2',11),('3','3',12),('4','4',13);
 
--- ALL类型; 数据量大, 且查询字段不在索引上;
-EXPLAIN SELECT *  FROM test.`test_index` WHERE createTime>='2020-10-10 10:00:00' AND createTime<='2020-10-10 11:00:00' ;
-EXPLAIN SELECT name2  FROM test.`test_index` WHERE createTime>='2020-10-10 10:00:00' AND createTime<='2020-10-10 11:00:00' ;
+-- type:index; where字段走索引,select字段命中索引; 只需取索引上的数据
+EXPLAIN SELECT id FROM user_t WHERE age >= 0 AND age <= 99;
+-- type:range; where字段走索引; 遍历表部分数据
+EXPLAIN SELECT nickName FROM user_t WHERE age>= 0 AND age <= 99;
 
-#
-# 1. channelId deviceId 有组合索引
-# 1.1 走索引
-EXPLAIN SELECT * FROM test.`test_index` WHERE name2 = 'cat'; # 符合前缀索引
-EXPLAIN SELECT * FROM test.`test_index` WHERE name1 = 'cat' AND name2 = 'cat'; # 符合前缀索引, 顺序不一样, update更新也走索引
-# 1.2 全表扫描(rows=10), 结果只有一个, filter= 10%;
-EXPLAIN SELECT * FROM test.`test_index` WHERE name1 = 'cat';
-# 1.3 全表扫描, 结果全部, filter= 100%; 给表所有数据 加锁
-EXPLAIN UPDATE test.`test_index` SET name2=null WHERE name2 = 'cat';
+### in 与 exist
+# 在mysql中,in有限制,exist没有
+# null值不参与in或not in,也就是说in and not in 并不是全量值,排除了null值
+
+# 因为in是可以走索引的,但in中数据量过大索引就会失效;# 适合B表比A表数据小的情况;
+explain select * from user_t where age in (1); # type: ref
+explain select * from user_t where age in (1,2); # type:range
+explain select * from user_t where age in (1,2,4,5,6,7,8,9); # type:ALL; in中数据量过大
+explain select * from user_t where age not in (10,11); # type: range; 数据在索引中
+-- 索引在乎where字段再索引范围内?? 走索引需要更详细的mysql优化逻辑;
+explain select * from user_t where age not in (1,2); # type: ALL; 数据不在索引中
+explain select id from user_t where age not in (1,2); # type: index; ??
+explain select * from user_t where age not in (1,2,4,5,6,7,8,9); # type:ALL
+explain select * from user_t where age in (select id from user_t);
+explain select * from user_t where age not in (select id from user_t);
+
+# exist 先执行外层,然后执行内层
+-- type:ALL,type:index
+explain select * from user_t where exists (select 1 from user_t where user_t.age = user_t.age);
+-- type:ALL,type:ALL
+explain select * from user_t where exists (select 1 from user_t where user_t.nickName = user_t.nickName);
+-- type:ALL,type:const
+explain select * from user_t where exists (select 1 from user_t where user_t.id = 1);
